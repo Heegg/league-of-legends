ebc3d7267929d442e15b217936818543f84ea62e
heegg <heegg4@gmail.com>
2024-08-26 02:05:10 -0400

    navigate btw different pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import ChampionsList from './components/ChampionsList';
+import MyChampions from './components/MyChampions';
+import AddChampions from './components/NewChampion';
+import Skins from './components/DamwonSkins';
 import Footer from './Footer';
+
 import Container from 'react-bootstrap/Container';
-import { Navbar, Nav } from 'react-bootstrap';
 import lolLogo from './logos/lolLogo.png'
+import { Navbar, Nav } from 'react-bootstrap';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 function App() {
   return (
+  <Router>
     <div className="App">
-      <Navbar 
+      <Navbar         
         bg="dark" 
         data-bs-theme="dark"
         expand="md" 
         className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">
+          <Navbar.Brand as={Link} to="/">
             <img 
               src={lolLogo} 
               alt='lolLogo'
@@ -24,17 +33,25 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#champions">Champions</Nav.Link>
-              <Nav.Link href="#mychampions">My Champions</Nav.Link>
-              <Nav.Link href="#addchampions">Add Champions</Nav.Link>
-              <Nav.Link href="#dwgskins">DWG Skins</Nav.Link>
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/champions">Champions</Nav.Link>
+              <Nav.Link as={Link} to="/mychampions">My Champions</Nav.Link>
+              <Nav.Link as={Link} to="/addchampions">Add Champions</Nav.Link>
+              <Nav.Link as={Link} to="/dwgskins">DWG Skins</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/champions" component={ChampionsList} />
+          <Route path="/mychampions" component={MyChampions} />
+          <Route path="/addchampions" component={AddChampions} />
+          <Route path="/dwgskins" component={Skins} />
+        </Switch>
       <Footer />
     </div>
+</Router>  
   );
 }
 
--- src/components/ChampionsList.js
+++ src/components/ChampionsList.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ChampionsList = () => {
   return (
     <div>
-      
+      ChampionsList
     </div>
   )
 }
