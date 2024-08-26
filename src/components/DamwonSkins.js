8385c8cef9df1dbe8814ebdf10157e6894e08bb4
heegg <heegg4@gmail.com>
2024-08-26 11:05:43 -0400

    DamwonSkins.css header

--- src/components/DamwonSkins.js
+++ src/components/DamwonSkins.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import './DamwonSkins.css'
+import logoDWG from '../logos/logoDWPurple.png'
 
 const DWGSkins = () => {
+
+
+
+
   return (
-    <div>
+    <div className='damwonSkins'>
+      <img src={logoDWG} alt="DWGLogo" className="DWLogo" />
       <hr/>
             <b><h1>Damwon Gaming 2020 Worlds Skins</h1></b>
       <hr/>
