b18738ff2728a24a764f08a357ad83076bbc6808
heegg <heegg4@gmail.com>
2024-08-30 12:24:56 -0400

    add sample video in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,15 @@ import './Home.css'
 const Home = () => {
   return (
     <div className='home'>
+        <iframe 
+        width="560" 
+        height="315" 
+        src="https://www.youtube.com/embed/UOxkGD8qRB4" 
+        title="YouTube video player" 
+        frameBorder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowFullScreen>
+      </iframe>
     </div>
   )
 }
