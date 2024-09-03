e026fac04cd39b8cccb16426b68c3c2464ff372a
heegg <heegg4@gmail.com>
2024-09-03 11:15:39 -0400

    add demo video to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Home.css'
+import myVideo from '../logos/lolVideo.mp4'
 
 const Home = () => {
   return (
     <div className='home'>
-      Home
+      <video controls>
+        <source src={myVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   )
 }
